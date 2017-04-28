3e57dbbdd5913829247ef6a0b2faa172597db60a
bizlemdev <bizlemdev@gmail.com>
2017-04-28 11:29:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,42 @@ restService.post('/bizbotapi', function(req, res) {
        speech = "in Which tool you want to create Project?";
       }else if(req.body.result.parameters.echoText == "Github"){
       speech = "What is the Project Name and  Description?";
+      }else if(req.body.result.parameters.echoText == "Name: New, Description: NLP POC"){
+      speech = "Ok. I am going to create \"New\" project in Github with description \"NLP POC\"";
+      }else if(req.body.result.parameters.echoText == "How many Tasks open in X Project current sprint"){
+      speech = "20 Tasks and 5 Bugs are in Open status";
+      }else if(req.body.result.parameters.echoText == "Use X project"){
+      speech = "ok";
+      }else if(req.body.result.parameters.echoText == "Start Build"){
+      speech = "What is the Job name that you want to start building?";
+      }else if(req.body.result.parameters.echoText == "Ansibe API"){
+      speech = "Build Process Completed";
+      }else if(req.body.result.parameters.echoText == "Create New Branch in Github and CI/CD pipeline"){
+      speech = "Which Project?";
+      }else if(req.body.result.parameters.echoText == "X Project"){
+      speech = "What should be new branch Name?";
+      }else if(req.body.result.parameters.echoText == "beta_v0.1"){
+      speech = "Is new pipeline for \"beta_v0.1\" Branch?";
+      }else if(req.body.result.parameters.echoText == "yes"){
+      speech = "Is the pipeline for developer or Autodeployment?";
+      }else if(req.body.result.parameters.echoText == "Autodeployment"){
+      speech="Creating \"beta_v0.1\" Branch in Github for \"X\" repository";
+            	speech=speech+'<br>'+"Creating new CI/CD pipeline job for \"beta_v0.1\" Branch with default Autodeployment schedule";
+            	speech=speech+'<br>'+"CI/CD pipeline job (\"beta_v0.1\") created in Jenkins";
+      }else if(req.body.result.parameters.echoText == "Create New Task"){
+      speech = "in Which project you want to create task?";
+      }else if(req.body.result.parameters.echoText == "\"X\""){
+      speech = "What is the Task Description?";
+      }else if(req.body.result.parameters.echoText == "NLP POC"){
+      speech = "Do you want to assign it to specific user?";
+      }else if(req.body.result.parameters.echoText == "I am not sure at this point"){
+      speech="Ok. I am going to create \"NLP POC\" task under \"X\" project and assign it to default user.";
+      speech=speech+'<br>'+"X-1023(\"NLP POC\") is created under \"X\" project.";
+      }else if(req.body.result.parameters.echoText == "Assign NLP POC Task to Geetanjali"){
+      speech = " is it for \"X\" project?";
+      }else if(req.body.result.parameters.echoText == "ya"){
+      speech="Ok. I am Assigning \"NLP POC\" task under \"X\" project to \"Geetanjali\".";
+      speech=speech+'<br>'+"X-1023(\"NLP POC\") is assigned to \"Geetanjali\".";
       }else{
       speech = "I am not able to Recognize your Voice !";
       }
